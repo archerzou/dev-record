95b873a221ab32418c1a8e353dce1b27b254f9b5
Zou Zhefeng <archer.zou84@gmail.com>
2026-04-30 09:27:44 +1200

    finish update, patch and delete actions for habit

--- DevRecord/DevRecord.Api/Program.cs
+++ DevRecord/DevRecord.Api/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
 })
+.AddNewtonsoftJson()
 .AddXmlSerializerFormatters();
 
 builder.Services.AddOpenApi();
