87501a2807b905a6be16cf170d6664ee0fc9601c
Zou Zhefeng <archer.zou84@gmail.com>
2026-06-16 23:36:26 +1200

    fix the build error

--- DevRecord/DevHabit.IntegrationTests/Infrastructure/DevRecordWebAppFactory.cs
+++ DevRecord/DevHabit.IntegrationTests/Infrastructure/DevRecordWebAppFactory.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Testcontainers.PostgreSql;
 using WireMock.Server;
 
